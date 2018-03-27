415a2c052b1fa7d58bb6568ec2bfe825d99fefbe
Riddhi Mehta <rimeht@microsoft.com>
2018-03-27 14:35:20 -0700

    updated vector class with dot product and angle calculations

--- testVectors.py
+++ testVectors.py
@@ -1,6 +1,9 @@
 import vector as v
 
-def main():
+def testcase1():
+    print('#'*20)
+    print('performing arithmetic operations on vectors')
+    print('#'*20)
     vec1 = v.Vector([8.218, -9.341])
     vec2 = v.Vector([-1.129, 2.111])
     vec3 = v.Vector([7.119, 8.215])
@@ -9,6 +12,53 @@ def main():
     print(vec1 + vec2)
     print(vec3 - vec4)
     print(vec5 * 7.41)
+    print('#'*20)
+
+def testcase2():
+    print('#'*20)
+    print('getting magnitude and unit vectors for vectors provided')
+    print('#'*20)
+    # test vectors for magnitude test
+    vec1 = v.Vector([-0.221, 7.437])
+    vec2 = v.Vector([8.813, -1.331, -6.247])
+    # print results
+    print(vec1.getMagnitude())
+    print(vec2.getMagnitude())
+    # test vectors for unit vector test
+    vec3 = v.Vector([5.581, -2.136])
+    vec4 = v.Vector([1.996, 3.108, -4.554])
+    # print results
+    print(vec3.normalize())
+    print(vec4.normalize())
+    print('#'*20)
+
+def testcase3():
+    print('#'*20)
+    print('getting the dot product the angle between 2 vectors')
+    print('#'*20)
+    # test for dot product
+    v1 = v.Vector([7.887, 4.138])
+    w1 = v.Vector([-8.802, 6.776])
+    v2 = v.Vector([-5.955, -4.904, -1.874])
+    w2 = v.Vector([-4.496, -8.755, 7.103])
+    # print results
+    print(v1.dot(w1))
+    print(v2.dot(w2))
+    # test for the angle between 2 vectors
+    v3 = v.Vector([3.183, -7.627])
+    w3 = v.Vector([-2.668, 5.319])
+    v4 = v.Vector([7.35, 0.221, 5.188])
+    w4 = v.Vector([2.751, 8.259, 3.985])
+    # print results
+    print(v3.angle(w3, unit='radian'))
+    print(v4.angle(w4, unit='degree'))
+    print('#'*20)
+
+
+def main():
+    testcase1()
+    testcase2()
+    testcase3()
 
 if __name__ == '__main__':
     main()
--- vector.py
+++ vector.py
@@ -1,4 +1,9 @@
+import math
+
 class Vector():
+    
+    CANNOT_NORMALIZE_ZERO_VECTOR = 'Cannot normalize a zero vector'
+
     def __init__(self, coordinates):
         try:
             if not coordinates:
@@ -62,13 +67,13 @@ class Vector():
             raise TypeError('Can perform addition only with another vector or a scalar')
     
     def __mul__(self, v):
-        print(v)
         try:
             if isinstance(v, (int, float)):
                 expand = []
                 for _ in range(self.dimension):
                     expand.append(v)
                 v = Vector(expand)
+            if isinstance(v, Vector):
                 mul_vec = Vector([self.coordinates[i] * v.coordinates[i] for i in range(self.dimension)])
                 return mul_vec
             else:
@@ -77,3 +82,47 @@ class Vector():
             raise TypeError('Can only perform multiplication with a scalar with this function. To multiply 2 vectors use Vector.matmul()')
     
     __rmul__ = __mul__
+
+    def getMagnitude(self):
+        '''
+        Magnitude of a vector is the measure of it's length
+        rtype: Class 'vector'
+        '''
+        magnitude = math.sqrt(sum([i**2 for i in self.coordinates]))
+        return magnitude
+
+    def normalize(self):
+        '''
+        Normalizing a vector is the process of finding a unit vector i.e. 
+        a vector with magnitude 1 in the same direction as a given vector
+        rtype: vector
+        '''
+        magnitude = self.getMagnitude()
+        try:
+            unit_vecor = self * (1/magnitude)
+            return unit_vecor
+        except ZeroDivisionError:
+            raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR)
+
+    def dot(self, v):
+        '''
+        Calculating the dot product of  2 vectors i.e. 
+        v . w = v1*w1 + v2*w2 + ... + vn*wn
+        '''
+        dot = sum([u*v for u, v in zip(self.coordinates, v.coordinates)])
+        return dot
+    
+    def angle(self, v, unit='radian'):
+        try:
+            v1 = self.normalize()
+            w1 = v.normalize()
+            angle = math.acos(v1.dot(w1))
+            if unit == 'degree':
+                angle *= 180/math.pi
+            return angle
+
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR:
+                raise Exception('Cannot comput an angle with a zero vector')
+            else:
+                raise e
